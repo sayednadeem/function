9273dc1bc6e876fc18521236b6d557aecb1f1b9c
sayednadeem <111424944+sayednadeem@users.noreply.github.com>
2022-08-17 14:55:49 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,63 @@
+# def max_of_three(a, b, c):
+#     if a > b and a > c:
+#         return a
+#     elif b > a and b >c:
+#         return b
+#     else:
+#         return c
+#
+# def max_of_three(a,b,c):
+#     sum=(a,b,c)
+#     return sum
+#
+# num1=int(input("enter a number"))
+# sum=max_of_three(num1,2,3)
+# print(sum)
+#
+#
+#
+# def max_of_three(a,b,c):
+#     sum=(a,b,c)
+#     return sum
+#
+# num1=int(input("enter a number"))
+# sum=max_of_three(num1,2,3)
+# print(sum)
+#
+#
+#
+# def max_of_three(a,b,c):
+#     sum=(a,b,c)
+#     return sum
+#
+# num1=int(input("enter a number"))
+# sum=max_of_three(num1,2,3)
+# print(sum)
+#
+# #
+#
+# def max_of_three(a,b,c):
+#     if a>b and b>c:
+#         return a
+#    elif b>a and b>c:
+#         return b
+#    else:
+#        return c
+#
+#
+#
+# sum=max_of_three(num1,2,3)
+# print(sum)
+#
+
+
+def max_of_three(a,b,c):
+    if a>b and a>c:
+        return a
+    elif b>a and b>c:
+        return b
+    else:
+        return c
+
+sum=max_of_three(1,2,3)
+print(sum)
