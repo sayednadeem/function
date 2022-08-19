5aee768f071182dd66ac1f33a2c48b18f9dae5f6
sayednadeem <111424944+sayednadeem@users.noreply.github.com>
2022-08-19 15:33:27 +0530

    second commit

---
+++ withoutfunction.py
@@ -0,0 +1,38 @@
+print("*****      welcome to nadeem calculator    ********* ")
+print("enter the number1:")
+num1=int(input())
+print("enter the number2:")
+num2=int(input())
+print("these are the operaion you can:")
+print("1.Addition")
+print("2.subtraction")
+print("3.multiplication")
+print("4.division")
+print("5.modulus")
+a=input("please chooose an option (1,2,3,4,5:")
+def add(num1,num2):
+    return num1+num2
+if a=="1":
+    result=add(num1,num2)
+    print(" This is an substraction")
+    print(result)
+def add(num1,num2):
+    return num1+num2
+if a=="2":
+    result=add(num1,num2)
+    print(" This is an substraction")
+    print(result)
+def add(num1, num2):
+        return num1 + num2
+if a == "3":
+        result = add(num1, num2)
+        print(" This is an substraction")
+        print(result)
+def add(num1,num2):
+    return num1+num2
+if a=="4":
+    result=add(num1,num2)
+    print(" This is an substraction")
+    print(result)
+
+
